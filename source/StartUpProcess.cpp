6fa3dae1487b3748a271b87e90d5b15315481551
zcstrife <zcstrife@gmail.com>
2025-12-30 16:09:32 +0100

    Fade In Fade Out Logo + Background

--- source/StartUpProcess.cpp
+++ source/StartUpProcess.cpp
@@ -40,6 +40,7 @@ StartUpProcess::StartUpProcess()
 	GXImageData = Resources::GetImageData("gxlogo.png");
 	GXImage = new GuiImage(GXImageData);
 	GXImage->SetAlignment(ALIGN_CENTER, ALIGN_MIDDLE);
+	GXImage->SetAlpha(0);
 	GXImage->SetPosition(screenwidth / 2, screenheight / 2 - 50);
 
 	titleTxt = new GuiText("Loading...", 24, (GXColor){255, 255, 255, 255});
@@ -149,6 +150,36 @@ int StartUpProcess::ParseArguments(int argc, char *argv[])
 	}
 
 	return quickBoot;
+}
+
+void StartUpProcess::LogoFadeIn(GuiImage* logo, int step)
+{
+    if(!logo || step <= 0)
+        return;
+
+    for(int a = 0; a <= 255; a += step)
+    {
+        logo->SetAlpha(a);
+        Draw();
+    }
+    logo->SetAlpha(255);
+    Draw();
+}
+
+void StartUpProcess::FadeOutLogoAndBackground(GuiImage* logo, GuiImage* bg, int step)
+{
+    if(!logo || !bg || step >= 0)
+        return;
+
+    for(int a = 255; a >= 0; a += step) // step négatif
+    {
+        logo->SetAlpha(a);
+        bg->SetAlpha(a);
+        Draw();
+    }
+    logo->SetAlpha(0);
+    bg->SetAlpha(0);
+    Draw();
 }
 
 void StartUpProcess::TextFade(int direction)
@@ -528,11 +559,13 @@ void StartUpProcess::Draw()
 {
 	background->Draw();
 	GXImage->Draw();
+	LogoFadeIn(GXImage, 8);
 	//titleTxt->Draw();
 	//messageTxt->Draw();
 	//versionTxt->Draw();
 	if (drawCancel)
 		cancelTxt->Draw();
+	FadeOutLogoAndBackground(GXImage, background, -8);
 	Menu_Render();
 }
 
